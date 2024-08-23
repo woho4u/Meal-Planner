9de46ad260f26e40a393bf605d544b495549aa99
Oliver Eriksen <okebedrift@gmail.com>
2024-08-24 00:09:10 +0200

    add sidebar placeholder

---
+++ app/components/Sidebar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const sidebar = () => {
+  return (
+    <>
+      <div className="flex flex-col h-full w-fit px-3 gray2-bg absolute">
+        <div className="w-7 h-7 rounded-full gray1-bg"></div>
+        <div className="w-7 h-7 rounded-full gray1-bg"></div>
+        <div className="w-7 h-7 rounded-full gray1-bg"></div>
+      </div>
+    </>
+  );
+};
+
+export default sidebar;
